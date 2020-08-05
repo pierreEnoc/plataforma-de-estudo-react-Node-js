46f90a50eaceba129c32c40c498e51c8271c9c2c
pierreEnoc <pierre_enoc>
2020-08-05 20:15:19 -0300

    :sparkles:add header with component

--- src/assets/pages/TeachersList/index.tsx
+++ src/assets/pages/TeachersList/index.tsx
@@ -8,7 +8,24 @@ import  './styles.css';
 function TeacherList() {
     return (
        <div id="page-teacher-list" className="container">
-           <PageHeader title="Estes são os proffys disponiveis."/>
+           <PageHeader title="Estes são os proffys disponiveis.">
+             <form id="search-teachers">
+               <div className="imput-block">
+                    <label htmlFor="subject">Matéria</label>
+                    <input type="text" id="subject" />
+              </div> 
+
+              <div className="imput-block">
+                    <label htmlFor="subject">Dia da semana</label>
+                    <input type="text" id="week_day" />
+              </div> 
+
+              <div className="imput-block">
+                    <label htmlFor="subject">Hora</label>
+                    <input type="text" id="time" />
+              </div> 
+             </form>
+           </PageHeader>
        </div>
     )
 }

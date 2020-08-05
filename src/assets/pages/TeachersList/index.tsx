feaf31fb12d8c89c85c5e8fb61ffaf9b9ed52e2b
pierreEnoc <pierre_enoc>
2020-08-05 19:58:28 -0300

    :sparkles:use props

--- src/assets/pages/TeachersList/index.tsx
+++ src/assets/pages/TeachersList/index.tsx
@@ -8,7 +8,7 @@ import  './styles.css';
 function TeacherList() {
     return (
        <div id="page-teacher-list" className="container">
-           <PageHeader />
+           <PageHeader title="Estes são os proffys disponiveis."/>
        </div>
     )
 }
